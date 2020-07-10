596038219f0fe518be47f3ccdb4988b12a79e4e1
Qicz <qiczzhu@gmail.com>
2020-07-10 11:03:46 +0800

    add getRequestIP

--- src/main/java/org/openingo/jdkits/IPKit.java
+++ src/main/java/org/openingo/jdkits/IPKit.java
@@ -27,6 +27,7 @@
 
 package org.openingo.jdkits;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.*;
 import java.util.Enumeration;
 
@@ -83,5 +84,31 @@ public final class IPKit {
             e.printStackTrace();
         }
         return ip;
+    }
+
+    /**
+     * Get Request Ip Addr
+     */
+    public String getRequestIP(HttpServletRequest request) {
+        String unknown = "unknown";
+        String ipAddress;
+        ipAddress = request.getHeader("x-forwarded-for");
+        if (ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        // split with ','
+        if (ipAddress != null && ipAddress.length() > 15) {
+            // "***.***.***.***".length()= 15
+            if (ipAddress.indexOf(",") > 0) {
+                ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+            }
+        }
+        return ipAddress;
     }
 }
