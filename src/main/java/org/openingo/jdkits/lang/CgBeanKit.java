042a9d30bf68fc5c36021cfdb5ae6f6c6c78d401
Qicz <qiczzhu@gmail.com>
2020-09-10 13:28:06 +0800

    add CgBeanKit

---
+++ src/main/java/org/openingo/jdkits/lang/CgBeanKit.java
@@ -0,0 +1,125 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 OpeningO Co.,Ltd.
+ *
+ *    https://openingo.org
+ *    contactus(at)openingo.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.openingo.jdkits.lang;
+
+import com.esotericsoftware.reflectasm.ConstructorAccess;
+import net.sf.cglib.beans.BeanCopier;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * CgBeanKit
+ *
+ * @author Qicz
+ */
+public final class CgBeanKit {
+
+    private CgBeanKit() {
+        //do nothing
+    }
+
+    private static final Map<String, BeanCopier> BEAN_COPIER_CACHE = new ConcurrentHashMap<>();
+
+    private static final Map<String, ConstructorAccess> CONSTRUCTOR_ACCESS_CACHE = new ConcurrentHashMap<>();
+
+    public static void copyProperties(Object source, Object target) {
+        BeanCopier copier = getBeanCopier(source.getClass(), target.getClass());
+        copier.copy(source, target, null);
+    }
+
+    private static BeanCopier getBeanCopier(Class<?> sourceClass, Class<?> targetClass) {
+        String beanKey = generateKey(sourceClass, targetClass);
+        BeanCopier copier = null;
+        if (!BEAN_COPIER_CACHE.containsKey(beanKey)) {
+            copier = BeanCopier.create(sourceClass, targetClass, false);
+            BEAN_COPIER_CACHE.put(beanKey, copier);
+        } else {
+            copier = BEAN_COPIER_CACHE.get(beanKey);
+        }
+        return copier;
+    }
+
+    /**
+     * 两个类的全限定名拼接起来构成Key
+     *
+     * @param sourceClass
+     * @param targetClass
+     */
+    private static String generateKey(Class<?> sourceClass, Class<?> targetClass) {
+        return sourceClass.getName() + targetClass.getName();
+    }
+
+    public static <T> T copyProperties(Object source, Class<T> targetClass) {
+        T t = null;
+        try {
+            t = targetClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(String.format("Create new instance of %s failed: %s", targetClass, e.getMessage()));
+        }
+        copyProperties(source, t);
+        return t;
+    }
+
+    public static <T> List<T> copyPropertiesOfList(List<?> sourceList, Class<T> targetClass) {
+        if (sourceList == null || sourceList.isEmpty()) {
+            return Collections.emptyList();
+        }
+        ConstructorAccess<T> constructorAccess = getConstructorAccess(targetClass);
+        List<T> resultList = new ArrayList<>(sourceList.size());
+        for (Object o : sourceList) {
+            T t = null;
+            try {
+                t = constructorAccess.newInstance();
+                copyProperties(o, t);
+                resultList.add(t);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return resultList;
+    }
+
+    private static <T> ConstructorAccess<T> getConstructorAccess(Class<T> targetClass) {
+        ConstructorAccess<T> constructorAccess = CONSTRUCTOR_ACCESS_CACHE.get(targetClass.getName());
+        if (constructorAccess != null) {
+            return constructorAccess;
+        }
+        try {
+            constructorAccess = ConstructorAccess.get(targetClass);
+            constructorAccess.newInstance();
+            CONSTRUCTOR_ACCESS_CACHE.put(targetClass.toString(), constructorAccess);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Create new instance of %s failed: %s", targetClass, e.getMessage()));
+        }
+        return constructorAccess;
+    }
+}
---
+++ src/test/java/jdkit/demo/CgBeanKitDemo.java
@@ -0,0 +1,153 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 OpeningO Co.,Ltd.
+ *
+ *    https://openingo.org
+ *    contactus(at)openingo.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package jdkit.demo;
+
+import org.openingo.jdkits.lang.CgBeanKit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * CgBeanKitDemo
+ *
+ * @author Qicz
+ */
+public class CgBeanKitDemo {
+
+    public static void main(String[] args) {
+
+        Map m1 = new HashMap<>();
+
+        m1.put("a", "b");
+        Map m2 = new HashMap<>();
+
+        CgBeanKit.copyProperties(m1, m2);
+
+        System.out.println(m2);
+
+        A a = new A();
+        a.a1 = "a1";
+        a.a2 = "a2";
+        a.a3 = 11;
+
+        B b = new B();
+
+        CgBeanKit.copyProperties(a, b);
+
+        System.out.println(b);
+    }
+
+    static class A {
+        String a1;
+        String a2;
+        int a3;
+
+        public String getA1() {
+            return a1;
+        }
+
+        public void setA1(String a1) {
+            this.a1 = a1;
+        }
+
+        public String getA2() {
+            return a2;
+        }
+
+        public void setA2(String a2) {
+            this.a2 = a2;
+        }
+
+        public int getA3() {
+            return a3;
+        }
+
+        public void setA3(int a3) {
+            this.a3 = a3;
+        }
+    }
+
+    static class B {
+        String a1;
+        String a2;
+        int a3;
+        int b1;
+        int b2;
+
+        public String getA1() {
+            return a1;
+        }
+
+        public void setA1(String a1) {
+            this.a1 = a1;
+        }
+
+        public String getA2() {
+            return a2;
+        }
+
+        public void setA2(String a2) {
+            this.a2 = a2;
+        }
+
+        public int getA3() {
+            return a3;
+        }
+
+        public void setA3(int a3) {
+            this.a3 = a3;
+        }
+
+        public int getB1() {
+            return b1;
+        }
+
+        public void setB1(int b1) {
+            this.b1 = b1;
+        }
+
+        public int getB2() {
+            return b2;
+        }
+
+        public void setB2(int b2) {
+            this.b2 = b2;
+        }
+
+        @Override
+        public String toString() {
+            return "B{" +
+                    "a1='" + a1 + '\'' +
+                    ", a2='" + a2 + '\'' +
+                    ", a3=" + a3 +
+                    ", b1=" + b1 +
+                    ", b2=" + b2 +
+                    '}';
+        }
+    }
+}
